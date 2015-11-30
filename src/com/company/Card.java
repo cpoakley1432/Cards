db006dd99d17db45627062ca454d1713423006fa
cpoakley1432 <cpoakley1432@gmail.com>
2015-11-30 11:41:22 -0500

    create hands

--- src/com/company/Card.java
+++ src/com/company/Card.java
@@ -32,4 +32,23 @@ public class Card {
         this.suit = suit;
         this.rank = rank;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Card card = (Card) o;
+
+        if (suit != card.suit) return false;
+        return rank == card.rank;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = suit.hashCode();
+        result = 31 * result + rank.hashCode();
+        return result;
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,9 +16,38 @@ public class Main {
         return deck;
     }
 
+    static HashSet<HashSet<Card>> createHands(HashSet<Card> deck){
+        HashSet<HashSet<Card>> hands = new HashSet();
+        for (Card c1 : deck){
+            HashSet<Card> deck2 = (HashSet<Card>) deck.clone();
+            deck2.remove(c1);
+            for (Card c2 : deck2){
+                HashSet<Card> deck3 = (HashSet<Card>) deck2.clone();
+                deck3.remove(c2);
+                for (Card c3 : deck3){
+                    HashSet<Card> deck4 = (HashSet<Card>) deck3.clone();
+                    deck4.remove(c3);
+                    for (Card c4 : deck4){
+                    HashSet<Card> hand = new HashSet();
+                        hand.add(c1);
+                        hand.add(c2);
+                        hand.add(c3);
+                        hand.add(c4);
+                        hands.add(hand);
+                    }
+                }
+            }
+        }
+        return hands;
+    }
+
     public static void main(String[] args) {
+        Card aceOfSpades = new Card(Card.Suit.SPADES, Card.Rank.ACE);
         HashSet<Card> deck = createDeck();
-        System.out.println(deck.size());
+        //System.out.println(deck.size());
+        //System.out.println(deck.contains(aceOfSpades));
+        HashSet<HashSet<Card>> hands = createHands(deck);
+        System.out.println(hands.size());
     }
 
 }
